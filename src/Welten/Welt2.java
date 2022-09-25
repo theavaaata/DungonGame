e38bd232379c49bcd6cbf35168f100e9a78275d2
theavaaata <99283738+theavaaata@users.noreply.github.com>
2022-09-25 12:48:04 +0200

    von welt zu welt

--- src/Welten/Welt2.java
+++ src/Welten/Welt2.java
@@ -9,20 +9,6 @@ import static GFX.Variables.width;
 
 public class Welt2 {
     public static void main(){
-        if(Player.player.y >= Variables.height){
-            Variables.world = 5;
-            Player.player.y = 0;
-            Refresh.refresh();
-        }
-        if(Player.player.x > width){
-            Variables.world = 3;
-            Player.player.x = 0;
-            Refresh.refresh();
-        }
-        if(Player.player.x < -player.playerheight){
-            Variables.world = 1;
-            Player.player.x = width;
-            Refresh.refresh();
-        }
+
     }
 }
--- src/Welten/Welt5.java
+++ src/Welten/Welt5.java
@@ -9,20 +9,6 @@ import static GFX.Variables.width;
 
 public class Welt5 {
     public static void main(){
-        if(Player.player.y < -Player.player.playerheight){
-            Variables.world = 2;
-            Player.player.y = height;
-            Refresh.refresh();
-        }
-        if(Player.player.x > width){
-            Variables.world = 6;
-            Player.player.x = 0;
-            Refresh.refresh();
-        }
-        if(Player.player.x < -player.playerheight){
-            Variables.world = 4;
-            Player.player.x = width;
-            Refresh.refresh();
-        }
+
     }
 }
